9785fcb364cdce4938b2866fe32b9a925860035d
João Dias <joaovitorelf@gmail.com>
2023-03-07 22:59:09 -0300

    Finalizado

--- src/controller/KillController.java
+++ src/controller/KillController.java
@@ -10,92 +10,86 @@ public class KillController {
 	public KillController() {
 		super();
 	}
-	
+
 	// Método que retorna o Sistema Operacional
 	private String os = System.getProperty("os.name");
 
 	public String getOS() {
 		return os;
 	}
-	
+
 	// Método que lista os processos
 	public void listaProcessos(String process) {
-		if(os.contains("Windows")) {
+		if (os.contains("Windows")) {
 			String lista = "TASKLIST /FO TABLE";
 			callProcess(lista);
-		} else if(os.contains("Linux" )) {
+		} else if (os.contains("Linux")) {
 			String lista = "ps -ef";
 			callProcess(lista);
 		}
 	}
-	
+
 	// Método que mata um processo pelo PID e finaliza ele
+//	public void mataPid(int param) {
+//		String windows = "TASKKILL /PID";
+//		String linux = "kill -9";
+//		int pid = 0;
+//		StringBuffer buffer = new StringBuffer();
+//		if(os.contains("Windows")) {
+//			pid = param;
+//				buffer.append(windows);
+//				buffer.append(" ");
+//				buffer.append(pid);
+//				callProcess(buffer.toString());
+//		} else {
+//				buffer.append(linux);
+//				buffer.append(" ");
+//				buffer.append(pid);
+//				callProcess(buffer.toString());
+//			
+//		}
+//		
+//		
+//	}
 	public void mataPid(int param) {
 		String windows = "TASKKILL /PID";
 		String linux = "kill -9";
 		int pid = 0;
 		StringBuffer buffer = new StringBuffer();
-		if(os.contains("Windows")) {
-			pid = param;
-			try {
-				buffer.append(windows);
-				buffer.append(" ");
-				buffer.append(pid);
-				
-				callProcess(buffer.toString());
-			} catch (Exception e) {
-				String msgErro = e.getMessage();
-				System.out.println(msgErro);
-			}
+		if (os.contains("Windows")) {
+			pid = (param);
+			buffer.append(windows);
+			buffer.append(" ");
+			buffer.append(pid);
+			callProcess(buffer.toString());
 		} else {
-			try {
-				buffer.append(linux);
-				buffer.append(" ");
-				buffer.append(pid);
-				
-				callProcess(buffer.toString());
-			} catch (Exception e) {
-				String msgErro = e.getMessage();
-				System.out.println(msgErro);
-			}
+			pid = (param);
+			buffer.append(linux);
+			buffer.append(" ");
+			buffer.append(pid);
+			callProcess(buffer.toString());
 		}
-		
-		
 	}
-	
-	//Método que mata um processo pelo NOME e finaliza ele
+
+	// Método que mata um processo pelo NOME e finaliza ele
 	public void mataNome(String name) {
 		String windows = "TASKKILL /IM";
 		String linux = "pkill -f";
 		StringBuffer buffer = new StringBuffer();
-		if(os.contains("Windows")) {
-			
-			try {
-				buffer.append(windows);
-				buffer.append(" ");
-				buffer.append(name);
-				
-				callProcess(buffer.toString());
-			} catch (Exception e) {
-				String msgErro = e.getMessage();
-				System.out.println(msgErro);
-			}
+		if (os.contains("Windows")) {
+			buffer.append(windows);
+			buffer.append(" ");
+			buffer.append(name);
+			callProcess(buffer.toString());
 		} else {
-			
-			try {
-				buffer.append(linux);
-				buffer.append(" ");
-				buffer.append(name);
-				
-				callProcess(buffer.toString());
-			} catch (Exception e) {
-				String msgErro = e.getMessage();
-				System.out.println(msgErro);
-			}
+			buffer.append(linux);
+			buffer.append(" ");
+			buffer.append(name);
+			callProcess(buffer.toString());
 		}
-		
+
 	}
-	
+
 	// Método que executa um determinado processo
 	public void callProcess(String process) {
 		try {
@@ -104,8 +98,8 @@ public class KillController {
 			InputStreamReader leitor = new InputStreamReader(fluxo);
 			BufferedReader buffer = new BufferedReader(leitor);
 			String linha = buffer.readLine();
-			
-			while(linha != null) {
+
+			while (linha != null) {
 				System.out.println(linha);
 				linha = buffer.readLine();
 			}
@@ -116,6 +110,5 @@ public class KillController {
 			e.printStackTrace();
 		}
 	}
-	
 
 }
--- src/view/Main.java
+++ src/view/Main.java
@@ -10,26 +10,27 @@ public class Main {
 
 		KillController Kc = new KillController();
 
-		String process = Kc.getOS();
+		String process = "";
 		int n = 0;
-
+		int processP;
+		
 		while (n != 5) {
 			n = Integer.parseInt(JOptionPane.showInputDialog(
-					"Selecione uma opção: 1 para mostrar o sistema operacional | 2 para mostrar a lista de processos | 3 para matar um processo pelo PID | 4 para matar um processo pelo nome | 5 para finalizar o programa "));
+					"Selecione uma opção: \n 1 - para mostrar o sistema operacional \n 2 - para mostrar a lista de processos \n 3 - para matar um processo pelo PID \n 4 - para matar um processo pelo nome \n 5 - para finalizar o programa "));
 			switch (n) {
 			case 1:
-				JOptionPane.showMessageDialog(null, "Seu Sistema operacional é: " + process);
+				JOptionPane.showMessageDialog(null, "Seu Sistema operacional é: " + Kc.getOS());
 				break;
 			case 2:
 				JOptionPane.showMessageDialog(null, "Olhe para o console: ");
 				Kc.listaProcessos(process);
 				break;
 			case 3:
-				int pid = Integer.parseInt(JOptionPane.showInputDialog("Digite o PID do processo: "));
-				Kc.mataPid(pid);
+				processP = Integer.parseInt(JOptionPane.showInputDialog("Digite o PID do processo: "));
+				Kc.mataPid(processP);
 				break;
 			case 4:
-				String name = JOptionPane.showInputDialog("Digite o nome do processo: ");
+				String name = JOptionPane.showInputDialog(null, "Digite o nome do processo: ");
 				Kc.mataNome(name);
 				break;
 			case 5:
